0e77cf2441357c9206465a382d866bea2b2efa80
송성빈 <bvmoomvd@gmail.com>
2022-09-20 02:21:26 +0900

    [feature] 인기 검색어 엔티티 추가

---
+++ src/main/java/com/search/web/domain/PopularSearchTermRepository.java
@@ -0,0 +1,6 @@
+package com.search.web.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PopularSearchTermRepository extends JpaRepository<PopularSearchTerm, Long> {
+}
